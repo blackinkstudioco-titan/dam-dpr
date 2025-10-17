1481c5029f3d28224287311b9934ddbec0268374
titan <titan_bpkix@titans-MacBook-Pro.local>
2025-10-18 00:26:57 +0700

    Add anonymous component for frontend-layout

---
+++ resources/views/components/frontend-layout.blade.php
@@ -0,0 +1,9 @@
+{{-- resources/views/components/frontend-layout.blade.php --}}
+@props([])
+
+{{--
+  Komponen ini digunakan untuk menggantikan <x-frontend-layout>.
+  Ia akan merender layout utama dari resources/views/layouts/frontend.blade.php
+--}}
+
+@include('layouts.frontend')
