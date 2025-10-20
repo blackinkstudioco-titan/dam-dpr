56b53895263202f5df2c7d49208a2cffbe32e973
blackinkstudioco-titan <blackinkstudio.co@gmail.com>
2025-10-20 20:03:00 +0700

    add auth fitur artikel

--- routes/web.php
+++ routes/web.php
@@ -38,14 +38,19 @@ Route::middleware('auth')->group(function () {
 Route::middleware(['auth'])->group(function () {
     Route::get('/artikel', [ArtikelController::class, 'index'])->name('artikel.index');
     Route::get('/artikel', [ArtikelController::class, 'index'])->name('artikel.index');
-    Route::get('/artikel/create', [ArtikelController::class, 'create'])->name('artikel.create');
-    Route::post('/artikel', [ArtikelController::class, 'store'])->name('artikel.store');
-    Route::get('/artikel/{id}/edit', [ArtikelController::class, 'edit'])->name('artikel.edit');
-    Route::put('/artikel/{id}', [ArtikelController::class, 'update'])->name('artikel.update');
-    Route::delete('/artikel/{id}', [ArtikelController::class, 'destroy'])->name('artikel.destroy');
     Route::get('/artikel/{id}', [ArtikelController::class, 'show'])->name('artikel.show');
 });
-
+// Admin, Editor, Uploader bisa create
+Route::middleware(['role:admin,editor,uploader'])->group(function () {
+  Route::get('/artikel/create', [ArtikelController::class, 'create'])->name('artikel.create');
+  Route::post('/artikel', [ArtikelController::class, 'store'])->name('artikel.store');
+});
+//editor
+Route::middleware(['role:admin,editor,uploader'])->group(function () {
+  Route::get('/artikel/{id}/edit', [ArtikelController::class, 'edit'])->name('artikel.edit');
+  Route::put('/artikel/{id}', [ArtikelController::class, 'update'])->name('artikel.update');
+  Route::delete('/artikel/{id}', [ArtikelController::class, 'destroy'])->name('artikel.destroy');  
+});
 
 Route::middleware(['auth'])->group(function () {
   Route::middleware(['role:admin'])->group(function () {
